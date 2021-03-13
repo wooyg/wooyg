1e9bce335b917989153ee54e500e0e93d4355eb9
WooYG <wooyg@wooyg.com>
2021-03-13 09:57:51 +0900

    20210313-0957.정리

--- web/mapc-app/commonAdmin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-{ // BLOCK:get_common:20150825:Common/index.php 그대로 가져오기
-
-    $rootDir = __DIR__;
-    @include($rootDir . DS . '..' . DS . 'common' . DS . 'index.php');
-
-} // BLOCK
-
-// this is it
--- web/mapc-app/commonAdmin/views/users/load.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-type: application/json");
-
-$result = ['users' => $v['users']];
-
-echo json_encode($result);
-
-// this is it
